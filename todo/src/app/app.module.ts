8cfcf1282dc802fb67f9068eab5601df9a2f3c1e
Tainara Alves <tainara.nascimento16@etec.sp.gov.br>
2023-05-16 20:36:39 -0300

    Finalizado

---
+++ todo/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+import { AppComponent } from './app.component';
+import { TodolistComponent } from './todolist/todolist.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodolistComponent
+  ],
+  imports: [
+    BrowserModule,
+   
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
